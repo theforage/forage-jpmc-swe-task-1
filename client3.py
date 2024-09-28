8bf1f501534687b6b6be5402ca1597d47d202cbc
Ajit Kumar <stack.ajit@gmail.com>
2024-09-29 01:45:46 +0530

    updated price

--- client3.py
+++ client3.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
-        price{}
+        prices {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
             prices[stock]=price
